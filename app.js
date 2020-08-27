28d25c8dc95850a33a592192575b290bbf7c0768
Jay Park <jp6ud@virginia.edu>
2020-08-26 20:23:52 -0400

    set up multi-player sockets and disconnection

--- app.js
+++ app.js
@@ -11,20 +11,29 @@ serv.listen(2000);
 console.log("Server started.");
 var play_id = 0;
 
+
+var PLAYER_LIST = {};
 var io = require('socket.io')(serv,{});
-io.sockets.on('connection', function(socket){
-  play_id++;  
-  socket.emit('serverMsg',{
-    id: play_id,
+io.sockets.on('connection', function(player){
+  play_id++;
+  player.id = play_id;
+  player.name = "";
+  player.status = false;
+  player.deck = [];
+  player.revealed = [];
+  PLAYER_LIST[player.id] = player;
+  player.emit('serverMsg',{
+    id: player.id,
 	});
+
+  socket.on('disconnect', function(){
+    delete PLAYER_LIST[player.id];  
+  })
 
 	socket.on('happy',function(data){
 		console.log('happy because ' + data.reason);
 	});
- 
-	
- 
+
+
+
 });
- 
- 
- 
